3dc1fb352aacbc7ee062e241b119d6682db15e00
Tiago Peixoto <tiagopsilvad@gmail.com>
2024-12-15 00:27:54 -0300

    backend do status pronto

--- backend/routes/livros.js
+++ backend/routes/livros.js
@@ -49,14 +49,14 @@ router.post('/resenhas/postar', async (req, res) => {
   }
 });
 
-
-
 router.post('/status_do_livro/adicionar', async (req, res) => {
-  const { isbn, status, id_usuario } = req.body;
+  const { id, status, id_usuario } = req.body;
+
+  // Log dos dados recebidos
 
   // Verifica se todos os dados necessários foram enviados
-  if (!isbn || !status || !id_usuario) {
-    return res.status(400).send('Por favor, forneça isbn, status e id_usuario.');
+  if (!id || !status || !id_usuario) {
+    return res.status(400).send('Por favor, forneça id, status e id_usuario.');
   }
 
   // Verifica se o status é válido (de acordo com a tabela de status)
@@ -67,16 +67,63 @@ router.post('/status_do_livro/adicionar', async (req, res) => {
       return res.status(400).send('Status inválido.');
     }
 
-    // Insere os dados na tabela status_do_livro
-    const query = 'INSERT INTO livro_status (isbn, id_status, id_usuario) VALUES (?, ?, ?)';
-    const [results] = await pool.query(query, [isbn, status, id_usuario]);
+    // Insere os dados na tabela livro_status
+    const query = 'INSERT INTO livro_status (id, id_status, id_usuario) VALUES (?, ?, ?)';
+    const [results] = await pool.query(query, [id, status, id_usuario]);
 
     res.status(201).json({ message: 'Livro status atualizado com sucesso!', id: results.insertId });
   } catch (err) {
-    console.error('Erro ao inserir no status_do_livro: ', err);
+    console.error('Erro ao inserir no livro_status: ', err);
     res.status(500).send('Erro ao atualizar o status do livro.');
   }
 });
+
+
+
+
+
+router.post('/status', async (req, res) => {
+  const { id_status } = req.body; // Recebe o id_status do corpo da requisição
+
+  if (!id_status) {
+    return res.status(400).send('O parâmetro id_status é obrigatório.');
+  }
+
+  const query = `
+    SELECT ls.id, s.nome, u.id_usuario 
+    FROM livro_status ls
+    INNER JOIN usuarios u ON u.id_usuario = ls.id_usuario 
+    INNER JOIN status s ON s.id_status = ls.id_status 
+    WHERE s.id_status = ?;
+  `;
+
+  try {
+    const [results] = await pool.query(query, [id_status]); // Usa o parâmetro no SQL
+    res.json(results); // Retorna os resultados
+  } catch (err) {
+    console.error('Erro na consulta: ', err);
+    res.status(500).send('Erro ao buscar livros');
+  }
+});
+
+
+
+
+
+
+router.get('/getstatus', async (req, res) => {
+  let query = 'SELECT * from status;';
+
+  try {
+    const [results] = await pool.query(query);
+    res.json(results);
+  } catch (err) {
+    console.error('Erro na consulta: ', err);
+    res.status(500).send('Erro ao buscar livros');
+  }
+});
+
+
 
 
 
